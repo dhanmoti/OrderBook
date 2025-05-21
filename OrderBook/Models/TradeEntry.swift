6d9e3e258d59d947184c0aa716fdbf18c53e8426
Dhan Moti (Devi) <48517418+dhanmoti@users.noreply.github.com>
2025-05-21 16:14:05 +0800

    Fix duplicate id issue

--- OrderBook/Models/TradeEntry.swift
+++ OrderBook/Models/TradeEntry.swift
@@ -9,10 +9,16 @@ import Foundation
 
 struct TradeEntry: Identifiable {
     var id: String {
-        side.rawValue + "_\(timestamp.timeIntervalSince1970)" + "_\(price)"
+        side.rawValue + "_\(timestamp.timeIntervalSince1970)" + "_\(price)" + "_\(quantity)"
     }
     let side: Side
     let quantity: Int
     let timestamp: Date
     let price: Double
+}
+
+extension TradeEntry: Equatable {
+    static func == (lhs: TradeEntry, rhs: TradeEntry) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
